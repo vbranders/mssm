5e3e360f204f89e1e9a25030d71c8b4c5edf17c9
Branders Vincent <vbranders@gmail.com>
2018-09-21 11:18:11 +0200

    Fix install message

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,9 +5,9 @@
     #Test if the jar is required
     if(file.size(file.path(system.file(package="mssm"), "java/root.jar"))/(1024**2) < 1){
         cat("The JAR file is lighter than expected.\n
-                The actual JAR file used by the package will be downloaded and installed in the library `", system.file(package="mssm"),"`.\n
-                > If this message appears during installation, ignore it.
-                > If it appears during to package loading, you should contact the maintainer of the package.")
+    The actual JAR file used by the package will be downloaded and installed in the library `", system.file(package="mssm"),"`.\n
+    > If this message appears during installation, ignore it.
+    > If it appears during to package loading, you should contact the maintainer of the package.\n")
         download.file("https://github.com/vbranders/mssm/raw/master/inst/java/root.jar", destfile = file.path(system.file(package="mssm"), "java/root.jar"))
     }
     
