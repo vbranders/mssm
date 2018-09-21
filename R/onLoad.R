df7a8e4e573b1b4f25de2029039d32be6a3bdc3c
Branders Vincent <vbranders@gmail.com>
2018-09-21 09:44:10 +0200

    Update JAR path

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
     .jpackage(pkgname, lib.loc=libname)
-    .jaddLibrary('root', 'java/root.jar')
-    .jaddClassPath('java/root.jar')
+    # .jaddLibrary('root', 'inst/java/root.jar')
+    # .jaddClassPath('inst/java/root.jar')
 
     #Pourquoi peut pas en faire une methode accessible partout alors que tel quel c'est accessible partout ?
     # a = as.matrix(read.table('inst/names.txt', header = F, sep = "\t", row.names = 1, colClasses = "character"))
